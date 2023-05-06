6149f498d16de858b2e3391f2bbc4f8363364247
abdelmoatyyy <ahmedabdelmoatyy@gmail.com>
2023-05-06 14:35:19 +0200

    fixx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
       <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
-        <NextNprogress color="#nFC4F00" />
+        <NextNprogress color="#FC4F00" />
         <NavBar />
         <div className="flex gap-6 md:gap-20">
           <div className="h-[92vh] overflow-hidden xl:overflow-auto">
